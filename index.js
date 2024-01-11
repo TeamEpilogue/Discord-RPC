546199bff1d7c7b09e11b9f7badc35d76df98f39
obvTiger <83970850+obvTiger@users.noreply.github.com>
2024-01-11 10:52:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const client = require('discord-rich-presence')('1187307012665651200');
  
 client.updatePresence({
-  details: 'Coding The Website',
+  details: 'Join epilogue.social, the new (better) social media platform',
   startTimestamp: Date.now(),
   largeImageKey: 'img',
   instance: true,
